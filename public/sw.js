5a520731b82df3aad4901338ee8dd84811335a30
leonardosalles <leosallestm@gmail.com>
2026-08-20 01:53:49 -0300

    fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "screen2-me-v44";
+const CACHE_NAME = "screen2-me-v45";
 const ASSETS = ["/", "/styles.css", "/app.js", "/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
